f7f6113e153cd963eaf640ef6efe09cdf1413083
ToavinaMaria <ToavinaMaria>
2024-03-25 10:20:31 +0300

    fix : SoratraApplication

--- src/main/java/hei/school/soratra/endpoint/SoratraApplication.java
+++ src/main/java/hei/school/soratra/endpoint/SoratraApplication.java
@@ -1,3 +1,5 @@
+package hei.school.soratra.endpoint;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
